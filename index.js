ede2eba91f8ab334458a31fc0ae1635005953b9a
eva-freire <freiretrecueva@gmail.com>
2023-09-05 20:48:36 -0300

    subida de formulario sin funciones js

---
+++ index.js
@@ -0,0 +1,10 @@
+const url = `https://jsonplaceholder.typicode.com/users`;
+
+const form = document.getElementById('form');
+const name = document.getElementById('nombre');
+const lastName = document.getElementById('apellido');
+const birthDate = document.getElementById('fecha-de-nacimiento');
+const submitBtn = document.getElementById('submitBtn');
+
+
+
