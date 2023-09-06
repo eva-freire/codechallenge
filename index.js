54e9562cd30753dd7e1140062d64aa2618fc2520
eva-freire <freiretrecueva@gmail.com>
2023-09-05 22:32:45 -0300

    actualizo archivo js con funciones

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const url = `https://jsonplaceholder.typicode.com/users`;
 
 const form = document.getElementById('form');
-const name = document.getElementById('nombre');
-const lastName = document.getElementById('apellido');
-const birthDate = document.getElementById('fecha-de-nacimiento');
-const submitBtn = document.getElementById('submitBtn');
+
+let JSONBody = {};
 
 
+//Función que procesará los datos del formulario
+const procesaDatos = (event) => {
+event.preventDefault();
 
+// Para sacar los datos creo objeto (js) de tipo FormData q representa el formulario
+let formData = new FormData(event.target);
+//console.log(formData);
+
+//Object.fromEntries() es un metodo q transforma una lista de pares clave : valor en un objeto JS
+let completeData = Object.fromEntries(formData.entries());
+JSONBody=completeData;
+//console.log(JSONBody);
+
+};
+
+
+form.addEventListener('submit',function(e){
+    procesaDatos(e); 
+    
+    fetch(url , {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        }, body: JSON.stringify(JSONBody),
+    })
+    .then ((res) => {
+        if (!res.ok) {
+            throw new Error("Error en el fetch"); 
+        } return res.json(); 
+    })
+    .then ((data) => {
+        console.log ('Registro exitoso! Bienvenid@ ' , data.nombre, data);
+    })
+    .catch (error => {
+        console.log('Error: ' + error);
+    })
+});
